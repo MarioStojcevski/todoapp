c7ce1af539e2a2617e703c3ef878527b4fbf0c71
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-12-30 21:44:45 +0100

    created react project and added tailwind css

---
+++ to-do/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-600 text-3xl uppercase">
+        React To Do Application with MARIO
+      </h1>
+    </>
+  );
+}
+
+export default App;
